aa01d9e2eb3759a9d99f92f3f795d4f2504e3ca3
wmbotela <wmbotela@gmail.com>
2017-03-17 17:33:40 +0300

    this commit adds site html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+    $(document).ready(function(){
+      $('#searchName').submit(function(event){
+        event.preventDefault();
+        var userName = $('#user-name').val("");
+        $('.showUser').text("Yourname " + user-name + ".");
+});
+});
